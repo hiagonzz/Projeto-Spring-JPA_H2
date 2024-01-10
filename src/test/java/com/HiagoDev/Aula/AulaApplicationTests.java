5066b05552c070ed2444b0c7e3a8d988151100bc
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-10 03:07:41 -0300

    Iniciando o projeto

---
+++ src/test/java/com/HiagoDev/Aula/AulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HiagoDev.Aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
