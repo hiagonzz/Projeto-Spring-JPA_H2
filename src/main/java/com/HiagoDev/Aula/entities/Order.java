ea6eb3365305dc583c2aadeef22cb9ae85001110
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-12 11:46:03 -0300

    OrderStatus enum

--- src/main/java/com/HiagoDev/Aula/entities/Order.java
+++ src/main/java/com/HiagoDev/Aula/entities/Order.java
@@ -1,5 +1,6 @@
 package com.HiagoDev.Aula.entities;
 
+import com.HiagoDev.Aula.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -18,15 +19,19 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss 'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
-    public Order(Long id, Instant moment, User client) {
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+
     }
 
     public Order() {
@@ -47,6 +52,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
