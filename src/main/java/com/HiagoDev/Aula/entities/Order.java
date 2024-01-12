e9c21dbc9e03162b8de315ba90ed381ff0834418
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-12 01:36:32 -0300

    Order, instant, ISO 8601 - PT2

--- src/main/java/com/HiagoDev/Aula/entities/Order.java
+++ src/main/java/com/HiagoDev/Aula/entities/Order.java
@@ -1,22 +1,25 @@
 package com.HiagoDev.Aula.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
+
 @Entity
-@Table(name= "tb_order")
+@Table(name = "tb_order")
 public class Order implements Serializable {
 
-    private static  final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss 'Z'", timezone = "GMT")
     private Instant moment;
     @ManyToOne
-    @JoinColumn(name= "client_id")
+    @JoinColumn(name = "client_id")
     private User client;
 
     public Order(Long id, Instant moment, User client) {
--- src/main/java/com/HiagoDev/Aula/entities/User.java
+++ src/main/java/com/HiagoDev/Aula/entities/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -18,6 +19,7 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
