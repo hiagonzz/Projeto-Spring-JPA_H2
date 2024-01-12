21ba37888b499996ad85e84a12862ab41c96f7d1
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-12 00:02:20 -0300

    Service layer, component registration

---
+++ src/main/java/com/HiagoDev/Aula/services/UserService.java
@@ -0,0 +1,24 @@
+package com.HiagoDev.Aula.services;
+
+import com.HiagoDev.Aula.Repositories.UserRepository;
+import com.HiagoDev.Aula.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public  User findById(Long id){
+       Optional<User> obj = repository.findById(id);
+       return  obj.get();
+    }
+}
