d6a3ed366802f517b133452e289a007b4f421459
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-13 01:53:56 -0300

    Many-to-many association with extra attributes

--- src/main/java/com/HiagoDev/Aula/entities/Category.java
+++ src/main/java/com/HiagoDev/Aula/entities/Category.java
@@ -1,5 +1,6 @@
 package com.HiagoDev.Aula.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,7 +17,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
--- src/main/java/com/HiagoDev/Aula/entities/Product.java
+++ src/main/java/com/HiagoDev/Aula/entities/Product.java
@@ -18,7 +18,10 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgUrl;
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "produt_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
 
