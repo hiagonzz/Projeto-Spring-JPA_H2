963fe9d88c16c33036e5415506951664fb373f8f
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-15 11:38:59 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/HiagoDev/Aula/AulaApplication.java
+++ src/main/java/com/HiagoDev/Aula/AulaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AulaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AulaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AulaApplication.class, args);
+    }
 
 }
