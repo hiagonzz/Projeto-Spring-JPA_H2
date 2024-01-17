f7010aa536890fb3da313d28ace367a30f893144
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-16 23:40:05 -0300

    Exception handiling - update

--- src/main/java/com/HiagoDev/Aula/services/UserService.java
+++ src/main/java/com/HiagoDev/Aula/services/UserService.java
@@ -4,6 +4,7 @@ import com.HiagoDev.Aula.Repositories.UserRepository;
 import com.HiagoDev.Aula.entities.User;
 import com.HiagoDev.Aula.services.exceptions.DataBaseException;
 import com.HiagoDev.Aula.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e ){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
